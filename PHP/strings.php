b2504ac67e2c60d2f1534204ee34dc33562eea6a
arvydas <arvismola@gmail.com>
2018-08-07 21:26:47 +0300

    testas

---
+++ PHP/strings.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$x = 5;
+
+$s = 'Laba$xs';
+$o = "Ry\"ta($x)s";
+
+
+echo $s . ' ' .$o;
+echo '<br>';
+
+$str = <<<EOT
+dfgjmdfl;gk  gjdlkg jdfgkldf jglfdgj dfgjdfglfdgdflgkdfgdfgdfgjmdflkg
+gdfgfdlkgdfklgdfgldf
+gdfgjmdflkgdfkgld
+fgkjglkdfgjdflk;g
+EOT;
+
+echo $str;
+echo '<br>';
+
+echo strlen($str);
+echo '<br>';
+$s ="Labas rytas stai ir as";
+$m = explode(' ', $s);
+var_dump($m);
