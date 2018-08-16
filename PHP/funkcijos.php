afd493e45ca0a955b2f607a1f743cad7b6f1c97e
arvydas <arvismola@gmail.com>
2018-08-16 21:27:11 +0300

    testas

---
+++ PHP/funkcijos.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function f() {
+    echo 'Labas' . '<br>';
+}
+
+f(); f(); f();
+
+function kv($a) {
+    echo $a * $a;
+}
+
+kv(2); echo '<br>';
+kv(3); echo '<br>';
+kv(4); echo '<br>';
+kv(5); echo '<br>';
+
+function daug($a, $b) {
+    return $a * $b;
+}
+$x = 5 + 6 + daug(3,5); echo $x; '<br>';
