f920309daafe8c901cfe89b73c6815d8c227ea53
arvydas <arvismola@gmail.com>
2018-08-21 20:22:40 +0300

    testas

--- PHP/Namu darbai/namu darbas 9.php
+++ PHP/Namu darbai/namu darbas 9.php
@@ -3,17 +3,27 @@
 Sukurti funkciją - valiutos konverterį į EUR. Funkcijos parametrai: pinigai, valiuta. Turime sąrašą, kuriame yra pinigai (asociatyvinis masyvas): kiekis, valiuta. Padaryti naują masyvą, kuriame yra skaičiai (pinigai paversti i EUR). Išvesti su var_dump.
  */
 
-$v = [
-    ['valiuta' => 'litai', 'kiekis' => 1],
-    ['valiuta' => 'doleriai', 'kiekis' => 1],
-    ['valiuta' => 'svarai', 'kiekis' => 1],
-    ['valiuta' => 'zlotai', 'kiekis' => 1],
-];
-$k = ['litai' => 3.4528, 'doleriai' => 1.1420, 'svarai' => 0.8948, 'zlotai' => 4.3037];
-
-function konv ($pinigai, $valiuta) {
-    $m = [];
-    if ($valiuta == $v['litai'])
+function p($pin, $val) {
+    $eur=0;
+    switch ($val) {
+        case 'usd' : $eur=$pin/1.15; break;
+        case 'gbp' : $eur=$pin/0.89; break;
+        case 'lt' : $eur=$pin/3.45; break;
+        default : $eur= 'false';
+    }
+    return $eur;
 }
+
+$a= [
+    ['kiekis' => 100, 'valiuta' => 'usd'],
+    ['kiekis' => 100, 'valiuta' => 'gbp'],
+    ['kiekis' => 100, 'valiuta' => 'lt']
+];
+$b=[];
+foreach ($a as $konv) {
+    $b[]=p($konv['kiekis'],$konv['valiuta']);
+}
+var_dump($b);
+
 
 
