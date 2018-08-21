c2b202d036eaf811b708cfaee9a04f075cead7cd
arvydas <arvismola@gmail.com>
2018-08-21 19:34:59 +0300

    testas

--- PHP/Namu darbai/namu darbas 8.php
+++ PHP/Namu darbai/namu darbas 8.php
@@ -3,22 +3,12 @@
 Yra sąrašas automobilių, kur automobilis yra aprašytas tekstu, kur jo duomenys atskirti kableliais, pvz.: "Honda,Civic,2012,1000". Iš šio sąrašo sukurti naują sąrašą, kuriame automobilis būtų aprašytas asociatyviniu masyvu: gamintojas, modelis, metai, kaina.
  */
 
-echo '<ol>';
-echo '<li>' . "Honda,Civic,2012,1000" . '</li>';
+$m=['honda,civic,2010,3000'];
+$s=[];
 
-function masinos($mas){
-    $defaults = [
-        'gamintojas' => 'nepasirinkta',
-        'modelis' => 'nepasirinkta',
-        'metai' => 0,
-        'kaina'=> 0
-    ];
-    $m = [];
-    $options = array_merge($defaults, $mas);
-    for ($i=0; $i<$options['n']; $i++){
-        $m[] = ($options['start'], $options['stop']);
-    }
-    return $m;
+foreach ($m as $masina) {
+  $x=explode(',', $masina);
+  $s[]=['gamintojas'=>$x[0], 'modelis' => $x[1], 'metai' => $x[2], 'kaina' => $x[3],
+  ];
 }
-
-
+var_dump($s);
