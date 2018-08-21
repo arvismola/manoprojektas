0f47b6eeadb3337e7ccf0caf7a68972ec2b52ada
arvydas <arvismola@gmail.com>
2018-08-21 21:30:17 +0300

    testas

---
+++ PHP/Namu darbai/clases namu darbai 1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+sukurti klase kurioje yra savybe: masyvas su asmenimis, kur kiekvienas asmuo yra asociatyvinis masyvas : vardas, lytis. Sukurti 2 funkcijas, kurios paskaiciuotu kiek yra vyru ir kiek yra moteru, ir kad grazintu reiksmes
+ */
+
+class abc {
+    public $m = [
+        ['vardas' => 'Petras', 'lytis'=>'vyras'],
+        ['vardas' => 'Ona', 'lytis'=>'moteris'],
+        ['vardas' => 'Kazys', 'lytis'=>'vyras'],
+        ['vardas' => 'Mykolas', 'lytis'=>'vyras'],
+        ['vardas' => 'Zose', 'lytis'=>'moteris'],
+        ['vardas' => 'Marija', 'lytis'=>'moteris'],
+        ['vardas' => 'Albinas', 'lytis'=>'vyras'],
+        ['vardas' => 'Vaclovas', 'lytis'=>'vyras'],
+        ['vardas' => 'Janina', 'lytis'=>'moteris'],
+    ];
+
+}
