7a03eed14bf31752601afbe7abb7fe3c68049d1a
Arvis <arvismola@gmail.com>
2018-08-28 12:29:11 +0300

    namu darbai

---
+++ PHP/Namu darbai/clases namu darbai 3.php
@@ -0,0 +1,60 @@
+<?php
+/**
+Sukurti klasę su savybe - masyvu. Masyve asmenys, kurie aprasyti taip: vardas, sąskaita. Sukurti funkciją, kuri visiems Jonams suteiktų 100 EUR priedą Joninių proga. Pademonstruoti veikimą.
+ */
+
+class jon
+{
+    public $z = [
+        ['vardas' => 'Jonas', 'saskaita' => 0],
+        ['vardas' => 'Antanas', 'saskaita' => 0],
+        ['vardas' => 'Kazys', 'saskaita' => 0],
+        ['vardas' => 'Jonas', 'saskaita' => 0],
+        ['vardas' => 'Mykolas', 'saskaita' => 0],
+        ['vardas' => 'Petras', 'saskaita' => 0],
+        ['vardas' => 'Vaclovas', 'saskaita' => 0],
+        ['vardas' => 'Mindaugas', 'saskaita' => 0],
+        ['vardas' => 'Jonas', 'saskaita' => 0],
+        ['vardas' => 'Tadas', 'saskaita' => 0]
+    ];
+    function premija(){
+        $a = $this->z;
+        for ($i=0; $i<count($a); $i++){
+            if ($a[$i]['vardas'] == 'Jonas'){
+                $a[$i]['saskaita'] +=100;
+            }
+        }
+        //return $a;
+    }
+}
+
+
+$jon = new jon();
+$jon->premija();
+var_dump($jon);
+
+
+//neveikia
+
+/* $a=[
+    ['vardas' => 'Jonas', 'saskaita' => 0],
+    ['vardas' => 'Antanas', 'saskaita' => 0],
+    ['vardas' => 'Kazys', 'saskaita' => 0],
+    ['vardas' => 'Jonas', 'saskaita' => 0],
+    ['vardas' => 'Mykolas', 'saskaita' => 0],
+    ['vardas' => 'Petras', 'saskaita' => 0],
+    ['vardas' => 'Vaclovas', 'saskaita' => 0],
+    ['vardas' => 'Mindaugas', 'saskaita' => 0],
+    ['vardas' => 'Jonas', 'saskaita' => 0],
+    ['vardas' => 'Tadas', 'saskaita' => 0]
+];
+
+    for ($i=0; $i<count($a); $i++){
+        if ($a[$i]['vardas'] == 'Jonas')
+        { $a[$i]['saskaita'] += 100;}
+
+}
+var_dump($a); */ //sitas veikia
+
+
+
