73ef4297168d042267915a299d35ece36d06cce5
arvydas <arvismola@gmail.com>
2018-09-04 19:34:14 +0300

    testas

--- PHP/Namu darbai/namu darnas 12.php
+++ PHP/Namu darbai/namu darnas 12.php
@@ -3,10 +3,35 @@
 Sukurti skriptą, kuris surastu visus pirminius skaičius ("A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers" - https://en.wikipedia.org/wiki/Prime_number) nurodytame skaičių intervale nuo iki.
  */
 
-function prime($a, $b){
-    $m = [];
-    for ($i=$a; $i<count($b); $i++){
-        $m += $i;
-        for ($j=2; )
+class skaiciai {
+    static function tikrinti($sk){
+        $pirminis = true;
+        for ($i=2; $i<$sk-1; $i++){
+            if($sk % $i == 0){
+                $pirminis = false;
+            }
+        }
+        return $pirminis;
     }
+   static function pirminiai($from, $to){
+       $m = [];
+       for ($i=$from; $i<=$to; $i++){
+           if(skaiciai::tikrinti($i)) $m[] = $i;  ///arba galima kreiptis so tagu self::
+       }
+       return $m;
+   }
+}
+var_dump(skaiciai::pirminiai(10,20));
+
+
+
+
+
+
+
+
+
+
+function prime($a, $b){
+
 }
