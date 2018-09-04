66fa7149deb52b1062320a23c70fbc5c284683cb
arvydas <arvismola@gmail.com>
2018-09-04 19:19:22 +0300

    testas

--- PHP/Namu darbai/forms namu darbai/forms-nd-02-backend.php
+++ PHP/Namu darbai/forms namu darbai/forms-nd-02-backend.php
@@ -5,24 +5,8 @@ Sukurkite frontend failą su įvedimo forma. Forma turėtu turėti laukus : 3x t
 
 session_start();
 
-$_SESSION['salys'][] = [
-    'salies pavadinimas' => $_POST['salies pavadinimas'],
-    'sostine' => $_POST['sostine'],
-    'gyventoju skaicius' => $_POST['gyventoju skaicius'],
-    'salies tipas' => $_POST['salies tipas'],
-];
-
-function top3(){
-    usort($_SESSION['salys'], function($a,$b){
-        if ($a['gyventoju skaicius']>$b['gyventoju skaicius']) return 1;
-        elseif ($a['gyventoju skaicius']>$b['gyventoju skaicius']) return -1;
-        else return 0;
-    });
-    $m = [];
-    for ($i=0; $i<count($_SESSION['salys']) and $i<3; $i++){
-        $m[] = $_SESSION['salys'][$i];
-    }
-    return $m;
-}
-
+$_SESSION['salys'][] = $_POST;
 header("location: forms-nd-02-frontend.php");
+
+
+
--- PHP/Namu darbai/forms namu darbai/forms-nd-02-frontend.php
+++ PHP/Namu darbai/forms namu darbai/forms-nd-02-frontend.php
@@ -7,24 +7,37 @@
 </head>
 <body>
 <form method="post" action="forms-nd-02-backend.php">
-    <label>Šalies pavadinimas:</label><input type="text" name="salies pavadinimas"><br>
+    <label>Šalies pavadinimas:</label><input type="text" name="pavadinimas"><br>
     <label>Sostinės pavadinimas:</label><input type="text" name="sostine"><br>
-    <label>Gyventojų skaičius:</label><input type="text" name="gyventoju skaicius"><br>
+    <label>Gyventojų skaičius:</label><input type="text" name="skaicius"><br>
     <label>Šalies tipas:</label><br>
-    <label>Rytų:</label><input type="radio" name="salies tipas" value="rytu"><br>
-    <label>Vakarų:</label><input type="radio" name="salies tipas" value="vakaku"><br>
+    <label>Rytų:</label><input type="radio" name="tipas" value="rytu"><br>
+    <label>Vakarų:</label><input type="radio" name="tipas" value="vakaku"><br>
     <input type="submit" value="siusti">
-    <?php
-    echo '<table>';
-    for ($i=0; $i<count($_SESSION['salys']); $i++){
-        echo '<tr>';
-        echo '<td>' . $_SESSION['salys'][$i]['salies pavadinimas']  . '</td>';
-        echo '<td>' . $_SESSION['salys'][$i]['sostine']  . '</td>';
-        echo '<td>' . $_SESSION['salys'][$i]['gyventoju skaicius']  . '</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-    ?>
 </form>
+    <?php
+    if (Isset($_SESSION['salys'])){
+        echo '<table>';
+        usort($_SESSION['salys'], function($a, $b){
+           if ($a['skaicius'] > $b['skaicius']) return -1;
+           elseif ($a['skaicius'] < $b['skaicius']) return 1;
+           else return 0;
+        });
+        $i=0;
+        foreach ($_SESSION['salys'] as $salis) {
+            if ($i < 3) {
+                echo '<tr>';
+                echo '<td>' . $salis['pavadinimas'] . '</td>';
+                echo '<td>' . $salis['sostine'] . '</td>';
+                echo '<td>' . $salis['skaicius'] . '</td>';
+                echo '<td>' . $salis['tipas'] . '</td>';
+                echo '</tr>';
+            } else break;
+            $i++;
+        }
+        echo '</table>';
+    }
+    ?>
 </body>
 </html>
+
